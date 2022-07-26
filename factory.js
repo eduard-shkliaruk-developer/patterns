78e45b2dfc2608c593fe513ebd95a3080fb96f1b
Eduard <eduard.shkliaruk@globallogic.com>
2022-07-26 22:58:47 +0300

    Factory

---
+++ factory.js
@@ -0,0 +1,59 @@
+// Factory patern
+
+class Cat {
+    constructor(weight, name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    getAboutItself() {
+        console.log(`${this.name} has ${this.weight} kilo`);
+    }
+}
+
+class Dog {
+    constructor(weight, name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    getAboutItself() {
+        console.log(`${this.name} has ${this.weight} kilo`);
+    }
+}
+
+class Pig {
+    constructor(weight, name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    getAboutItself() {
+        console.log(`${this.name} has ${this.weight} kilo`);
+    }
+}
+
+function factory(weight, name) {
+    let Animal;
+
+    if (weight < 10) {
+        Animal = Cat;
+    }
+
+    if (weight >= 10 && weight < 20) {
+        Animal = Dog;
+    }
+
+    if (weight >= 20) {
+        Animal = Pig;
+    }
+
+    return new Animal(weight, name);
+}
+
+const nameAnimal = 'Pety'
+const weight = 9;
+
+const animal = factory(weight, nameAnimal);
+
+animal.getAboutItself();
